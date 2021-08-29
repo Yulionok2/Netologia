5980fa69ae8ca3ba4e2474e66accd19f8ad83efb
Yuliya Polozkova <yuliya.polozkova@mail.ru>
2021-08-29 16:01:38 +0300

    dz проармию и ЗЗ

--- 1.Fundamentals Python/Условные конструкции.Операции сравнения/DZ_1.py
+++ 1.Fundamentals Python/Условные конструкции.Операции сравнения/DZ_1.py
@@ -5,7 +5,7 @@ import re
 import sys
 
 if __name__ == '__main__':
-    n = int(raw_input().strip())
+    n = int(input().strip())
     if n%2!=0:
         print('Weird')
     elif n%2==0 and 2<n<5:
